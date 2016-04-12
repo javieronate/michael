a8b2d5e55f348fa4bb6c8e7ced1c47b7b8762b19
Michael Hasselberg <git@hasselbit.com>
2016-04-12 16:19:25 -0500

    config.php sample

---
+++ config-sample.php
@@ -0,0 +1,16 @@
+<?php
+
+// Configuracion MySQL
+$servidor = "";
+$mysqluser = "";
+$mysqldb = "";
+
+
+// ideas en https://api.wordpress.org/secret-key/1.1/salt/
+// Guarda una copia del SALT en un lugar seguro para recuperarlo en caso de perdida!
+$salt = "7?4E`]/j|Ocq]H6XMh,w(=]Xn >-pO6J8s,WTYb+C{ip!hIf|jdwM|67z!Z#*2d?C;|,8~lZM7^-Z!T7i/+}YVVtIKGU?HAFtM[j/:Wd:V[";
+
+// Carpeta de Archivos guardados (de preferencia fuera del public_html, wwww)
+$uploadfolder = "";
+
+?>
